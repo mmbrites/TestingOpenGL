5f63928df4008911277778a55c2111d1cf544b8c
mmbrites <miguel.martins.2b@gmail.com>
2025-01-11 19:34:09 +0000

    Writing Shaders...

--- FirstSteps/src/main.cpp
+++ FirstSteps/src/main.cpp
@@ -3,6 +3,61 @@
 
 #include <iostream>
 
+static unsigned int compileShader(unsigned int type, const std::string & source)
+{
+    
+    unsigned int id = glCreateShader(type);
+    const char * src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char * message = (char *) alloca(length * sizeof(char)); // Since C++ does not let you declare a C-style array with an arbitrary length, and you want to declare it on the stack, you can use alloca -- the only problem arises when the array is too big, since it may cause a stack overflow.
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader!" << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+
+    }
+
+    return id;
+
+}
+
+/*
+    vertexShader - vertex shader source code
+    fragmentShader - fragment shader source code
+*/
+static unsigned int createShader(const std::string & vertexShader, const std::string & fragmentShader)
+{
+
+    // Code necessary to compile both shaders
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    // Frees memory - although there are some that like to argue that it is inneffective in case you want to debug the shader source code
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+
+}
+
 int main(void)
 {
    
@@ -57,6 +112,30 @@ int main(void)
     // 0 --> the index of the vertex attribute to be enabled
     glEnableVertexAttribArray(0);
 
+    // This should have been done in a file, but whatever
+    std::string vertexShader = 
+        "#version 330 core\n" // Specifies the version of the GLSL (Shading Language). 'core' means that you cannot use deprecated functions
+        "\n"
+        "layout(location = 0) in vec4 position;\n" // 'location' --> Index of the attribute. We use vec4 instead of a vec2 because 'gl_Position' is a vec4
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+     std::string fragmentShader = 
+        "#version 330 core\n" // Specifies the version of the GLSL (Shading Language). 'core' means that you cannot use deprecated functions
+        "\n"
+        "layout(location = 0) out vec4 color;\n" // We want to output a 'color'
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n" // Color: red
+        "}\n";
+    
+    unsigned int shader = createShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -73,6 +152,8 @@ int main(void)
         glfwPollEvents();
     
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
